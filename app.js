e6f2d3c3caa6534bc716e195bb65c654df2351f5
Grøttum <grotar@egmont.com>
2017-11-24 12:58:51 +0100

    Friday sign off...

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var reacteer = require('./routes/reacteer');
-
 var app = express();
 
 // view engine setup
--- routes/reacteer.js
+++ routes/reacteer.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const schedule = require('node-schedule');
 const createPuppeteerPool = require('puppeteer-pool');
 const reacteer = require('./reacteerUtils/reacteerUtils.js');
-const fs = require('fs');
+
 
 const pool = createPuppeteerPool({
   max: 10,
@@ -23,8 +23,8 @@ const j = schedule.scheduleJob('*/15 * * * * *', async () => {
   if(!lock) {
     lock = true;
     console.log('lock not set - run routine . . . ');
-    const report = await reacteer.linkStats("", pool);
-    await write2file("", report);
+    const report = await reacteer.puppeteer.linkStats("", pool);
+    await reacteer.utils.write2file("", report);
     lock = false;
   }else{
     console.log('lock is set - drop routine . . . ');
@@ -39,31 +39,14 @@ const j = schedule.scheduleJob('*/15 * * * * *', async () => {
   await reacteer.takeScreenshot("teknologi", pool);*/
 });
 
-const write2file = async (section, data) => new Promise((resolve, reject) => {
-  fs.writeFile("public/json/" + section + '.json', JSON.stringify(data), (err) =>{
-      if(err){
-        reject(err);
-      } else {
-        resolve(true);
-      }
-  });
-});
-
-const rct = async () => new Promise((resolve, reject) => {
-  setTimeout(() => resolve('react'), 5000)
-});
-
-
 router.get('/', function(req, res, next) {
   let fend = rct();
   fend.then((f) => res.json({frontend: f , backend: xprss()}));
 });
 
 router.get('/linkstats', function(req, res, next) {
-  let lstats = runLinkstats();
-
-  lstats.then((data) => res.json(JSON.stringify(data)));
+  const jsn = readFromFile('public/json/.json');
+  res.json(JSON.stringify(jsn));
 });
-
 
 module.exports = router;
